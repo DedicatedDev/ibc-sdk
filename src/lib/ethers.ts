ce44aee53b237a939716d143b2591404b49be333
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2023-06-28 18:52:02 +0200

    utils: add better error messaging around eth provider

--- src/lib/ethers.ts
+++ src/lib/ethers.ts
@@ -1,7 +1,15 @@
 import * as ethers from 'ethers'
+import { getLogger } from './utils'
+
+const log = getLogger()
 
 export function newJsonRpcProvider(url: string) {
-  return new ethers.providers.JsonRpcProvider({ url: url, skipFetchSetup: true })
+  try {
+    return new ethers.providers.JsonRpcProvider({ url: url, skipFetchSetup: true })
+  } catch (e) {
+    log.error(`could not get eth provider with url '${url}': ${e}`)
+    throw e
+  }
 }
 
 export function addressify(addr: string): string {
